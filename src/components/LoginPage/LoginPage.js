7570a5803853792ddbd360a1cd51e55aaf2e88d2
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-21 13:45:46 +0300

    add-redirect-when-login

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 class LoginPage extends React.Component {
   constructor(props) {
@@ -15,29 +16,30 @@ class LoginPage extends React.Component {
   render() {
     const { username, password } = this.state;
     const { isAuth } = this.props;
-    return isAuth
-      ? <div>
-          <span>Вы успешно авторизированны!</span>
-        </div>
-      : <React.Fragment>
-          <form action="" onSubmit={this.handleSubmit}>
-            <input
-              type="text"
-              name="username"
-              placeholder={"Имя"}
-              onChange={this.handleChange}
-              value={username}
-            />
-            <input
-              type="text"
-              name="password"
-              placeholder={"Пароль"}
-              onChange={this.handleChange}
-              value={password}
-            />
-            <button type="submit">Log in</button>
-          </form>
-        </React.Fragment>;
+    if (isAuth) {
+      return <Redirect to="/profile" />;
+    }
+    return (
+      <React.Fragment>
+        <form action="" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder={"Имя"}
+            onChange={this.handleChange}
+            value={username}
+          />
+          <input
+            type="text"
+            name="password"
+            placeholder={"Пароль"}
+            onChange={this.handleChange}
+            value={password}
+          />
+          <button type="submit">Log in</button>
+        </form>
+      </React.Fragment>
+    );
   }
 
   handleSubmit = e => {
