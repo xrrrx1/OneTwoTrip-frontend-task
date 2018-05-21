f77ae37b82136f8458c2648207f7cae5475b5705
Valeriy Saphonov <xxrrrxx@yandex.ru>
2018-05-21 13:41:38 +0300

    add-redirect

--- src/containers/ProfilePageContainer.js
+++ src/containers/ProfilePageContainer.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import ProfilePage from "../components/ProfilePage/ProfilePage";
+import { Redirect } from "react-router-dom";
 
 class ProfilePageContainer extends React.Component {
   render() {
-    const { name } = this.props;
+    const { name, isAuth } = this.props;
+    if (!isAuth) {
+      return <Redirect to="/" />;
+    }
     return <ProfilePage name={name} />;
   }
 }
 
 export default connect(
   store => ({
-    name: store.session.name
+    name: store.session.name,
+    isAuth: Boolean(store.session.name)
   }),
   {}
 )(ProfilePageContainer);
